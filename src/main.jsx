6bf23cb7c63fc02c47044bb29702c5fa8d238196
Khaos-Code <makindeoluwatumininu@gmail.com>
2025-04-10 16:13:48 +0100

    vyhbjn

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/TechSphere">
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
   <AppContextProvider>
       <App />
